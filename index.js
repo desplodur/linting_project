af389f3156a03456acb673ba842c42d73430ad67
Laurenz Rudolph <Laurenz.Rudolph@t-online.de>
2022-05-12 10:04:13 +0200

    install husky

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const button = document.querySelector("button");
+const button = document.querySelector('button');
 
-button.addEventListener("click", () => {
-  alert("You clicked me!");
+button.addEventListener('click', () => {
+  alert('You clicked me!');
 });
